b6dea615891a59553d55b4f6f3d68b3ae37d111f
hachimanhiki <mh512158475@gmail.com>
2021-05-30 23:53:10 +0800

    feat: update upload page

---
+++ public/javascripts/index.js
@@ -0,0 +1,27 @@
+/*
+$('#file').change(function () {
+    var file = $('#file')[0].files[0];
+    var reader = new FileReader;
+    reader.onload = function (e) {
+        $('#demo').attr('src', e.target.result);
+    };
+    reader.readAsDataURL(file);
+});
+*/
+
+$(document).ready(function () {
+    $("#send").click(function () {
+        if ($("#name").val() == "") {
+            alert("你尚未填寫姓名");
+            eval("document.form1['name'].focus()");
+        } else if ($("#beginPrice").val() == "") {
+            alert("你尚未填寫電話");
+            eval("document.form1['beginPrice'].focus()");
+        } else if ($("#time").val() == "") {
+            alert("你尚未填寫地址");
+            eval("document.form1['time'].focus()");
+        } else {
+            document.form1.submit();
+        }
+    })
+});
